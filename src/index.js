fd63bd0f65d969fac7ad158ab32a64962b45ba1f
fieldr <fieldr92@gmail.com>
2018-11-22 14:54:57 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = { lat: null,  errorMessage: '' }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() { // called automatically when the component is first mounted
+    console.log('My component was rendered to the screen');
+    window.navigator.geolocation.getCurrentPosition( // two arguments are both CALLBACKS and thus make this function asnychronous
+      position => this.setState({ lat: position.coords.latitude }), // ALWAYS USE this.setState to set the state of a component // NEVER USE ASSIGNMENT
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  componentDidUpdate() { // called automatically each time the component is updated
+    console.log('My component was just updated - it rerendered!');
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+    return <Spinner message="Please accept location request..." />
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
