d724cf5d81eb69a7285d3ddf854351aa34c991f3
fieldr <fieldr92@gmail.com>
2019-05-22 17:33:34 +0100

    refactor again

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
 const App = () => {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [lat, errorMessage] = useLocation();
 
-  const renderContent = () => {
-    if (errorMessage && !lat) {
-      return <div>Error: {errorMessage}</div>;
-    }
-    if (!errorMessage && lat) {
-      return <SeasonDisplay lat={lat} />;
-    }
-    return <Spinner message="Please accept location request..." />;
-  };
+  let content;
+  if (errorMessage && !lat) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (!errorMessage && lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request..." />;
+  }
 
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      position => setLat(position.coords.latitude),
-      err => setErrorMessage(err.message)
-    );
-  }, []);
-
-  return <div className="border red">{renderContent()}</div>;
+  return <div className="border red">{content}</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
